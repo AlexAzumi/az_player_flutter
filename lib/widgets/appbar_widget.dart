79ddf6d85a6b7b06d6e678d968fe393d40545d1f
Alejandro Suárez <alejandro-hdez115@outlook.com>
2023-02-09 19:09:57 -0600

    Add some trailing commas in `appbar_widget.dart`

--- lib/widgets/appbar_widget.dart
+++ lib/widgets/appbar_widget.dart
@@ -5,22 +5,24 @@ class AppbarWidget extends StatelessWidget {
 
   Widget _menuItem(String menuText) {
     return Padding(
-        padding: const EdgeInsets.all(14.0),
-        child: Text(
-          menuText.toUpperCase(),
-          style: const TextStyle(color: Colors.white, fontSize: 16.0),
-        ));
+      padding: const EdgeInsets.all(14.0),
+      child: Text(
+        menuText.toUpperCase(),
+        style: const TextStyle(color: Colors.white, fontSize: 16.0),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Theme.of(context).primaryColor,
-        child: Row(
-          children: [
-            _menuItem('File'),
-            _menuItem('Sort'),
-          ],
-        ));
+      color: Theme.of(context).primaryColor,
+      child: Row(
+        children: [
+          _menuItem('File'),
+          _menuItem('Sort'),
+        ],
+      ),
+    );
   }
 }
