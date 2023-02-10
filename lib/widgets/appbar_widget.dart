f0e1e471ae514c55f697b6d736404d657058a2c5
Alejandro Suárez <alejandro-hdez115@outlook.com>
2023-02-09 18:40:48 -0600

    Add app bar items (sort of)

--- lib/widgets/appbar_widget.dart
+++ lib/widgets/appbar_widget.dart
@@ -3,8 +3,24 @@ import 'package:flutter/material.dart';
 class AppbarWidget extends StatelessWidget {
   const AppbarWidget({super.key});
 
+  Widget _menuItem(String menuText) {
+    return Padding(
+        padding: const EdgeInsets.all(14.0),
+        child: Text(
+          menuText.toUpperCase(),
+          style: const TextStyle(color: Colors.white, fontSize: 16.0),
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text('Hello there!');
+    return Container(
+        color: Theme.of(context).primaryColor,
+        child: Row(
+          children: [
+            _menuItem('File'),
+            _menuItem('Sort'),
+          ],
+        ));
   }
 }
