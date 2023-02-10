457e27a61c1e729c180d8a000792a267be9bb285
Alejandro Suárez <alejandro-hdez115@outlook.com>
2023-02-09 19:54:39 -0600

    Add app bar button items

--- lib/widgets/appbar_widget.dart
+++ lib/widgets/appbar_widget.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'dart:developer';
 
 class AppbarWidget extends StatelessWidget {
   const AppbarWidget({super.key});
 
-  Widget _menuItem(String menuText) {
+  Widget _menuItem(String menuText, void Function() onPressItem) {
     return Padding(
-      padding: const EdgeInsets.all(14.0),
-      child: Text(
-        menuText.toUpperCase(),
-        style: const TextStyle(color: Colors.white, fontSize: 16.0),
+      padding: const EdgeInsets.all(12.0),
+      child: TextButton(
+        onPressed: onPressItem,
+        child: Text(
+          menuText.toUpperCase(),
+          style: const TextStyle(color: Colors.white, fontSize: 16.0),
+        ),
       ),
     );
+  }
+
+  void onPressUpdateSongs() {
+    log('Pressed update songs');
+  }
+
+  void onPressSortByName() {
+    log('Pressed sort by name');
+  }
+
+  void onPressSortByArtist() {
+    log('Pressed sort by artist');
+  }
+
+  void onPressHelp() {
+    log('Pressed help');
+  }
+
+  // Exit button
+  void onPressedExit() {
+    exit(0);
   }
 
   @override
@@ -19,8 +46,11 @@ class AppbarWidget extends StatelessWidget {
       color: Theme.of(context).primaryColor,
       child: Row(
         children: [
-          _menuItem('File'),
-          _menuItem('Sort'),
+          _menuItem('Update songs', onPressUpdateSongs),
+          _menuItem('Sort by Name', onPressSortByName),
+          _menuItem('Sort by Artist', onPressSortByArtist),
+          _menuItem('Help', onPressHelp),
+          _menuItem('Exit', onPressedExit),
         ],
       ),
     );
